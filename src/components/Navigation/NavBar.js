4311bc27281abe3b04c8c8ddc06b6b95b4728e16
ameljohny <ameljohny.mail@gmail.com>
2021-06-29 23:55:22 +1000

    added navbar

---
+++ src/components/Navigation/NavBar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Image, Menu } from 'antd';
+import { useState } from 'react';
+import {
+  MailOutlined,
+  SettingOutlined,
+  LogoutOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import logo from 'logo.svg';
+
+const { SubMenu } = Menu;
+export default function NavBar() {
+  const [mail, setMail] = useState('');
+  const handleClick = (e) => {
+    console.log('click ', e);
+    setMail(e.key);
+  };
+
+  return (
+    <>
+      <Menu onClick={handleClick} selectedKeys={[mail]} mode='horizontal'>
+        <Menu.Item className='App-logo'>
+          <img src={logo} className='logo' alt='logo' key='logo' />
+        </Menu.Item>
+        <Menu.Item key='messages' icon={<MailOutlined />}>
+          Messages
+        </Menu.Item>
+        {/* <Menu.Item key="app" disabled icon={<AppstoreOutlined />}>
+              Navigation Two
+            </Menu.Item> */}
+
+        <SubMenu key='SubMenu' icon={<SettingOutlined />} title='Settings'>
+          <Menu.ItemGroup>
+            <Menu.Item key='Profile' icon={<UserOutlined />}>
+              Profile
+            </Menu.Item>
+          </Menu.ItemGroup>
+          <Menu.ItemGroup>
+            <Menu.Item key='LogOut' icon={<LogoutOutlined />}>
+              Logout
+            </Menu.Item>
+          </Menu.ItemGroup>
+        </SubMenu>
+      </Menu>
+    </>
+  );
+}
