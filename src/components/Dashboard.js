0669a0ee33d250b51fc96cb9aab994e5fcd202e5
ameljohny <ameljohny.mail@gmail.com>
2021-06-29 23:57:15 +1000

    added layout

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,48 +1,48 @@
-import { Menu } from 'antd';
-import {
-  MailOutlined,
-  AppstoreOutlined,
-  SettingOutlined,
-} from '@ant-design/icons';
-import { useState } from 'react';
+import { Layout } from 'antd';
+import NavBar from './Navigation/NavBar';
 
-const { SubMenu } = Menu;
-
+const { Header, Footer, Sider, Content } = Layout;
 function Dashboard() {
-  const [mail, setMail] = useState('');
-  const handleClick = (e) => {
-    console.log('click ', e);
-    setMail(e.key);
-  };
-
   return (
-    <Menu onClick={handleClick} selectedKeys={[mail]} mode="horizontal">
-      <Menu.Item key="mail" icon={<MailOutlined />}>
-        Navigation One
-      </Menu.Item>
-      <Menu.Item key="app" disabled icon={<AppstoreOutlined />}>
-        Navigation Two
-      </Menu.Item>
-      <SubMenu
-        key="SubMenu"
-        icon={<SettingOutlined />}
-        title="Navigation Three - Submenu"
-      >
-        <Menu.ItemGroup title="Item 1">
-          <Menu.Item key="setting:1">Option 1</Menu.Item>
-          <Menu.Item key="setting:2">Option 2</Menu.Item>
-        </Menu.ItemGroup>
-        <Menu.ItemGroup title="Item 2">
-          <Menu.Item key="setting:3">Option 3</Menu.Item>
-          <Menu.Item key="setting:4">Option 4</Menu.Item>
-        </Menu.ItemGroup>
-      </SubMenu>
-      <Menu.Item key="alipay">
-        <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-          Navigation Four - Link
-        </a>
-      </Menu.Item>
-    </Menu>
+    <div className='container'>
+      <Layout>
+        <Header>
+          <NavBar></NavBar>
+        </Header>
+        <Layout className='App-header'>
+          {/* <Sider className='primary-color'>Sider</Sider> */}
+          <Content>Content</Content>
+        </Layout>
+        <Footer>fOOOOTER</Footer>
+      </Layout>
+    </div>
+    // <Menu onClick={handleClick} selectedKeys={[mail]} mode="horizontal">
+    //   <Menu.Item key="mail" icon={<MailOutlined />}>
+    //     Navigation One
+    //   </Menu.Item>
+    //   <Menu.Item key="app" disabled icon={<AppstoreOutlined />}>
+    //     Navigation Two
+    //   </Menu.Item>
+    //   <SubMenu
+    //     key="SubMenu"
+    //     icon={<SettingOutlined />}
+    //     title="Navigation Three - Submenu"
+    //   >
+    //     <Menu.ItemGroup title="Item 1">
+    //       <Menu.Item key="setting:1">Option 1</Menu.Item>
+    //       <Menu.Item key="setting:2">Option 2</Menu.Item>
+    //     </Menu.ItemGroup>
+    //     <Menu.ItemGroup title="Item 2">
+    //       <Menu.Item key="setting:3">Option 3</Menu.Item>
+    //       <Menu.Item key="setting:4">Option 4</Menu.Item>
+    //     </Menu.ItemGroup>
+    //   </SubMenu>
+    //   <Menu.Item key="alipay">
+    //     <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+    //       Navigation Four - Link
+    //     </a>
+    //   </Menu.Item>
+    // </Menu>
   );
 }
 
