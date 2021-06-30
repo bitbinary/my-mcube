d5db3d3dc15c591f27dc7271153b435149563262
ameljohny <ameljohny.mail@gmail.com>
2021-06-30 19:17:35 +1000

    adding basic logout

--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Menu } from 'antd';
+import { useDispatch } from 'react-redux';
+import actions from 'redux/Authenticate/actions';
 import { useState } from 'react';
 import {
   MailOutlined,
@@ -8,18 +10,31 @@ import {
   UserOutlined,
 } from '@ant-design/icons';
 import logo from 'logo.svg';
+import { useHistory } from 'react-router-dom';
 
 const { SubMenu } = Menu;
 export default function NavBar() {
+  //Variables
+  const history = useHistory();
   const [mail, setMail] = useState('');
+  const dispatch = useDispatch();
+
+  //Functions
   const handleClick = (e) => {
     setMail(e.key);
   };
+  const logout = () => {
+    dispatch({
+      type: actions.LOGOUT,
+    });
+    history.push('/login');
+  };
 
+  //Render
   return (
     <>
       <Menu onClick={handleClick} selectedKeys={[mail]} mode='horizontal'>
-        <Menu.Item className='App-logo'>
+        <Menu.Item className='App-logo' key='applogo'>
           <img src={logo} className='logo' alt='logo' key='logo' />
         </Menu.Item>
         <Menu.Item key='messages' icon={<MailOutlined />}>
@@ -36,7 +51,11 @@ export default function NavBar() {
             </Menu.Item>
           </Menu.ItemGroup>
           <Menu.ItemGroup>
-            <Menu.Item key='LogOut' icon={<LogoutOutlined />}>
+            <Menu.Item
+              key='LogOut'
+              icon={<LogoutOutlined />}
+              onClick={() => logout()}
+            >
               Logout
             </Menu.Item>
           </Menu.ItemGroup>
--- src/redux/Authenticate/reducer.js
+++ src/redux/Authenticate/reducer.js
@@ -3,18 +3,24 @@ import actions from 'redux/Authenticate/actions';
 const initialState = {
   isAuthenticated: false,
   loader: false,
-}
+};
 
 function Reducer(state = initialState, action) {
   switch (action.type) {
     case actions.LOGIN:
-      return {...state, loader: true}
+      return { ...state, loader: true };
+    case actions.LOGOUT:
+      return { ...state, isAuthenticated: false, loader: false };
     case actions.LOGIN_SUCCESS:
-      return {...state, isAuthenticated: !!action.response.data.token, loader: false}
+      return {
+        ...state,
+        isAuthenticated: !!action.response.data.token,
+        loader: false,
+      };
     case actions.LOGIN_FAILURE:
-      return {...state, isAuthenticated: false, loader: false}
+      return { ...state, isAuthenticated: false, loader: false };
     default:
-      return state
+      return state;
   }
 }
 
