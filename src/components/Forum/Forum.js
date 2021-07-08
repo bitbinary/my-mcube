de9fdcb647372eae495258b5cf25c83213ef6722
ameljohny <ameljohny.mail@gmail.com>
2021-07-09 04:39:45 +1000

    removed console log

--- src/components/Forum/Forum.js
+++ src/components/Forum/Forum.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ForumPage from './ForumPage';
 import ForumLayout from './ForumLayout';
 export default function Forum() {
-  console.log('loading Forum');
   return (
     <ForumLayout>
       <ForumPage />
