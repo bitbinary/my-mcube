4e5be3ccaf4d2cb1e6a8fb44fc323726bf19f97a
ameljohny <ameljohny.mail@gmail.com>
2021-07-11 00:16:38 +1000

    login/registration page updated

---
+++ src/components/Authentication/RegisterPage.js
@@ -0,0 +1,35 @@
+import { Card, Tabs } from 'antd';
+import RegisterForm from './RegisterForm';
+const { TabPane } = Tabs;
+
+function RegisterPage() {
+  function callback(key) {
+    console.log(key);
+  }
+  return (
+    <>
+      {/* <PageHeader
+        className='site-page-header'
+        title='Registration'
+        subTitle='Welcome Onboard'
+      /> */}
+      <div className='container-login'>
+        <Card
+          title='Registration'
+          style={{ margin: 'auto', borderRadius: '8px' }}
+        >
+          <Tabs defaultActiveKey='mentee' onChange={callback}>
+            <TabPane tab='Mentee' key='mentee'>
+              <RegisterForm userType='mentee' />
+            </TabPane>
+            <TabPane tab='Mentor' key='mentor'>
+              <RegisterForm userType='mentor' />
+            </TabPane>
+          </Tabs>
+        </Card>
+      </div>
+    </>
+  );
+}
+
+export default RegisterPage;
