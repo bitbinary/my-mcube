242636360d6501970577d727fc24548fafab71f0
ameljohny <ameljohny.mail@gmail.com>
2021-08-07 22:29:21 +1000

    message WIP

--- src/redux/Messages/reducer.js
+++ src/redux/Messages/reducer.js
@@ -12,7 +12,10 @@ const initialState = {
 function Reducer(state = initialState, action) {
   switch (action.type) {
     case actions.GETCONTACTS_SUCCESS:
-      return { ...state, contacts: action.contacts };
+      if (!shallowCompare(state.contacts, action.contacts))
+        return { ...state, contacts: [...action.contacts] };
+      else return state;
+
     case actions.GETMESSAGES_SUCCESS:
       let newMessages = { ...state.messages };
       newMessages[action.toUser] = action.messages;
@@ -24,11 +27,25 @@ function Reducer(state = initialState, action) {
       };
     case actions.POSTMESSAGE_SUCCESS:
       return { ...state, contacts: action.contacts };
-    case actions.POSTMESSAGE_FAILURE:
-      return { ...state, contacts: action.contacts };
+    // case actions.POSTMESSAGE_FAILURE:
+    //   return { ...state, contacts: action.contacts };
     default:
       return state;
   }
 }
 
 export default Reducer;
+
+const shallowCompare = (a, b) => {
+  for (var key in a) {
+    if (!(key in b)) {
+      return false;
+    }
+  }
+  for (var key in b) {
+    if (!(key in a)) {
+      return false;
+    }
+  }
+  return true;
+};
